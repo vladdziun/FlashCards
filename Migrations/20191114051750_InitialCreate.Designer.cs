866f5a3e6a76109d0dd8dfbd64c98e353af4a41b
Vladyslav Dziun <v.dziun@gmail.com>
2019-11-18 20:42:55 -0800

    changed UI

--- Migrations/20191114051750_InitialCreate.Designer.cs
+++ Migrations/20191114051750_InitialCreate.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using LoginReg.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LoginReg.Migrations
 {
     [DbContext(typeof(LoginRegContext))]
-    partial class LoginRegContextModelSnapshot : ModelSnapshot
+    [Migration("20191114051750_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
