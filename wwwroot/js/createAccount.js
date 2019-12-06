6c72cf6192370173842f8682169b980a69e56d7b
Vladyslav Dziun <v.dziun@gmail.com>
2019-12-05 16:57:39 -0800

    added response for mobile devices

---
+++ wwwroot/js/createAccount.js
@@ -0,0 +1,8 @@
+function createAccount() {
+    var x = document.getElementById("createAccount");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
